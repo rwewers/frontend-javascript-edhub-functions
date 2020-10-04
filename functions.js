c0dbabc8d42df1678b835433375323b141af5eef
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-10-04 22:28:56 +0200

    Made answers to exercises

--- functions.js
+++ functions.js
@@ -4,7 +4,12 @@
 // 3, 4, 5 geeft 12
 // 11, 3, 9 geeft 23
 
+function sum(x, y, z) {
+    return x + y + z;
+}
 
+// om de uitkomst te kunnen zien moet de functie-aanroep nog wel omwikkelen met een console.log()
+console.log(sum(11,3, 9));
 
 /* Opdracht 2 */
 // Schrijf een functie die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
@@ -12,7 +17,11 @@
 // ---- Verwachte uitkomsten:
 // "abra", "cadabra" geeft "abracadabra"
 
+function wizard(first, second) {
+    return first + second;
+}
 
+wizard("abra", "cadabra");
 
 /* Opdracht 3 */
 // Schrijf een functie die twee parameters verwacht: een woord en een karakter
@@ -22,7 +31,11 @@
 // "beep", "_" geeft "_beep_"
 // "kaas", "Q" geeft "QkaasQ"
 
+function surround(word, character) {
+    return character + word + character;
+}
 
+surround("bril", "*");
 
 /* Opdracht 4 */
 // Schrijf een functie die een array van strings verwacht. De functie geeft
@@ -32,7 +45,17 @@
 // ["abra", "cadabra"] geeft "abracadabra"
 // ["a", "b", "c"] geeft "abc"
 
+function concatenate(wordArray) {
+    let output = "";
 
+    for (let i = 0; i < wordArray.length; i++) {
+        output = output + wordArray[i];
+    }
+
+    return output;
+}
+
+concatenate(["a", "b", "c"]);
 
 /* Opdracht 5 */
 // Schrijf een functie die een array van strings verwacht en een gezamelijke groet teruggeeft
@@ -41,3 +64,25 @@
 // ["Piet", "Henk"] geeft "Hoi Piet en Henk!"
 // ["A", "B", "C", "D", "E", "F"] geeft "Hoi A, B, C, D, E, en F!"
 
+function greeting(stringsArray) {
+    let greeting = "Hoi ";
+
+    for (let i = 0; i < stringsArray.length; i++) {
+        // als dit de laatste entry is, voeg dan het uitroepteken toe
+        if (i === stringsArray.length - 1) {
+            return greeting = greeting + "en " + stringsArray[i] + "!";
+        }
+        // als de array maar twee namen bevat, voeg dan geen komma toe
+        if (stringsArray.length === 2) {
+            greeting = greeting + stringsArray[i] + " ";
+        }
+        // als de array meer dan twee namen bevat, voeg dan wel een komma toe
+        if (stringsArray.length > 2) {
+            greeting = greeting + stringsArray[i] + ", ";
+        }
+    }
+
+    return greeting;
+}
+
+greeting(["Piet", "Henk"]);
